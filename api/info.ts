bdb288c18a85f0d9851908e79b87b58d9cb0b9bd
SegaraRai <SegaraRai@users.noreply.github.com>
2019-11-03 08:38:51 +0900

    最終更新日時を表示する

---
+++ api/info.ts
@@ -0,0 +1,21 @@
+import { NowRequest, NowResponse } from '@now/node';
+
+import { config } from './_utils/config';
+import { get } from './_utils/kvs';
+import { logWrapper } from './_utils/logWrapper';
+
+
+export default logWrapper(async (req: NowRequest, res: NowResponse) => {
+  if (req.method !== 'GET') {
+    res.status(405).send('405');
+    return;
+  }
+
+  const lastUpdate = parseInt(String(await get(config.lastUpdateKey)), 10);
+
+  const data = {
+    lastUpdate: isNaN(lastUpdate) ? null : lastUpdate,
+  };
+
+  res.json(data);
+});
