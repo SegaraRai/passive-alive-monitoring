31f1a3e43446341e3a65547df091db7f4d5cc876
SegaraRai <SegaraRai@users.noreply.github.com>
2019-11-03 09:57:58 +0900

    チャンネル名の設定ミスを修正

--- api/_actions/slack.ts
+++ api/_actions/slack.ts
@@ -7,7 +7,7 @@ import { ActionData } from '.';
 const postConfig = [
   (actionData: ActionData) => ({
     username: 'PAM bot',
-    channel: `#-${actionData.device.id}'`,
+    channel: `#-${actionData.device.id}`,
     text: `@here **${actionData.device.id}** is **${actionData.status.type}** now!\n(${actionData.status.strDuration}, since ${actionData.status.strSince})`,
   }),
   (actionData: ActionData) => ({
