d2bbcdb992336babce67e50b51e3d18162971c32
SegaraRai <SegaraRai@users.noreply.github.com>
2019-11-03 09:39:35 +0900

    formatText関数を修正

--- api/_utils/format.ts
+++ api/_utils/format.ts
@@ -1,11 +1,11 @@
 export function formatText(formatter: string, object: any) {
-  return formatter.replace(/\{\{(.*?)\}\}/, (_, key) => {
+  return formatter.replace(/\{\{(.*?)\}\}/g, (_, key) => {
     let o = object;
     for (const k of key.split('.')) {
       if (!o || !o.hasOwnProperty(k)) {
         return 'undefined';
       }
-      o = object[k];
+      o = o[k];
     }
     return String(o);
   });
